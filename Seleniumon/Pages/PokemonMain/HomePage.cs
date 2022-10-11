931d610ed355ec1e9714dd30e2b92ef458381598
Steven Kennedy <Kennedy.Steven.P@gmail.com>
2022-10-11 12:41:11 -0600

    Add project files.

---
+++ Seleniumon/Pages/PokemonMain/HomePage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seleniumon.Pages.PokemonMain
+{
+    internal class HomePage
+    {
+        //URL for Main site:https://www.pokemoncenter.com/ 
+    }
+}
