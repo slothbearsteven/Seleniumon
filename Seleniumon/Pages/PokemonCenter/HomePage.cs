b8710a185c9a7d8586ec6648031871575a996a4d
Steven Kennedy <Kennedy.Steven.P@gmail.com>
2022-10-13 07:59:05 -0600

    Not much. made navigation method to pokemon center

--- Seleniumon/Pages/PokemonCenter/HomePage.cs
+++ Seleniumon/Pages/PokemonCenter/HomePage.cs
@@ -9,5 +9,15 @@ namespace Seleniumon.Pages.PokemonCenter
     internal class HomePage
     {
         //URL for pokemon center (shop): https://www.pokemoncenter.com/
+        private IWebDriver driver;
+        public void Homepage(IWebDriver webDriver)
+        {
+            this.driver =  webDriver;
+        }
+
+        public void GoToPage()
+        {
+            driver.Navigate().GoToUrl("https://www.pokemoncenter.com/");
+        }
     }
 }
