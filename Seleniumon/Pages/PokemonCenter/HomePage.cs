8b3f4eed2da2b736be8078688e0e2ed59288091f
Steven Kennedy <Kennedy.Steven.P@gmail.com>
2022-10-17 10:08:16 -0600

    added navbar items to home page of Pokecenter

--- Seleniumon/Pages/PokemonCenter/HomePage.cs
+++ Seleniumon/Pages/PokemonCenter/HomePage.cs
@@ -15,6 +15,21 @@ namespace Seleniumon.Pages.PokemonCenter
             this.driver =  webDriver;
         }
 
+        //Begin Nav Bar items
+        public IWebElement newReleasesButton => driver.FindElement(By.Id("new-releases"));
+
+        public IWebElement plushButton => driver.FindElement(By.Id("plush"));
+        public IWebElement figuresPinsButton => driver.FindElement(By.Id("figures-pins"));
+        public IWebElement tcgButton => driver.FindElement(By.Id("trading-card-game"));
+        public IWebElement clothingButton => driver.FindElement(By.Id("clothing"));
+        public IWebElement homeItemsButton => driver.FindElement(By.Id("home-office"));
+        public IWebElement videoGameButton => driver.FindElement(By.Id("video-game"));
+
+        //limited time, remove after October 31st
+        public IWebElement halloweenButton => driver.FindElement(By.Id("halloween"));
+
+        //End Nav Bar items
+
         public void GoToPage()
         {
             driver.Navigate().GoToUrl("https://www.pokemoncenter.com/");
